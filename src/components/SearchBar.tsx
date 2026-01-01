1dfd733b5953ecd09b0a519b78e851c5f2197a3d
Lucas-L-Rodrigues <lucaslima.48a@gmail.com>
2026-01-01 10:54:34 -0300

    implementação da função de busca com o form

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -11,4 +11,17 @@ export function SearchBar() {
             navigate('/search?q=${term}')
         }
     }
+
+    return (
+        <form onSubmit={handleSearch} style={{ display: 'flex', alignItems: 'center',}}>
+            <input
+                type="text"
+                placeholder="Buscar na loja"
+                value={term}
+                onChange={e => setTerm(e.target.value)}
+                style={{padding: '5px', borderRadius: '4px', border: '1px solid #000000'}}
+            />
+            <button type="submit" style={{marginLeft: '5px'}}>🔎</button>
+        </form>
+    )
 }
