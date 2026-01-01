94f05114fc1ee2cab9a8493379e6b2807b638e8b
Lucas-L-Rodrigues <lucaslima.48a@gmail.com>
2026-01-01 10:52:24 -0300

    Removida a SearchBar do Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,11 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export function Layout() {
+    
+    //const [term, setTerm] = useState('')
+    //const navigate = useNavigate()
+
     return (
         <div>
             <nav style={{padding: '20px', background: '#B7B7B7'}} >
