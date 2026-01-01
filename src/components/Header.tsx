36fcd5768ea65267e88942ebc8e24503b53cd9cc
Lucas-L-Rodrigues <lucaslima.48a@gmail.com>
2026-01-01 12:05:59 -0300

    Organizado o header com as posições corretas dos elementos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,24 @@ import { SearchBar } from "./SearchBar";
 
 export function Header () {
     return (
-        <nav>
-            <div style={{marginLeft: 'auto', display: 'flex', gap: '15px'}}>
+        <nav style={{
+            padding:'15px',
+            background: '#D9D9D9',
+            display: 'flex',
+            gap: '15px',
+            alignItems: 'center',
+            borderBottom: '1px solid #000000'
+        }}>
+            <div style={{marginRight: 'auto', display: 'flex', gap: '15px'}}>
                 <Link to='/'>☕</Link>
                 <Link to='/Cart'>Carrinho</Link>
             </div>
 
-            <div style={{marginLeft: '20px'}}>
+            <div style={{justifyContent: 'center'}}>
                 <SearchBar />
             </div>
 
-            <Link to='/Login'>Login</Link>
+            <Link to='/Login' style={{marginLeft: 'auto'}}>Login</Link>
         </nav>
     )
 }
