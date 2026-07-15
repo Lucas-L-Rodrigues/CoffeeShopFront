cbb661878561fda7bab56e21bdb654fad03e38aa
Lucas-L-Rodrigues <lucaslima.48a@gmail.com>
2026-07-15 11:03:46 -0300

    Importando o Header para uma página "Home"

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../../components/layout/Header";
+
+export function Home() {
+    return (
+        <>
+            <Header />
+
+            <main className="container">
+                <h1> CoffeeShop</h1>
+
+                <p>Testando o Header</p>
+            </main>
+        </>
+    );
+}
